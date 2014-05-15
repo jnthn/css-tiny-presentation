da8373551a598f8623af929ec1e0c3be4575c0ad
Jonathan Worthington <jnthn@jnthn.net>
2014-05-15 17:33:43 +0200

    Saner lookaheads
    The lookahead syntax has changed. It's a little longer - but a lot
    easier to figure out what it means!

--- Tiny.pm
+++ Tiny.pm
@@ -25,7 +25,7 @@ method read_string($string is copy) {
 	$string ~~ s:g/ \s ** 2..* | '/*' .+? '*/' / /;
 
 	# Split into styles
-	for $string.split(/(?<=\})/).grep(/\S/) {
+	for $string.split(/<?after '}'>/).grep(/\S/) {
 		unless ( /^\s*([^{]+?)\s*\{(.*)\}\s*$/ ) {
 			return $self->_error( "Invalid or unexpected style data '$_'" );
 		}
